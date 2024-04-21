bea1a14865629c0640b0a735ee1e308da86da361
DouiriEya <douirieya9@gmail.com>
2024-04-21 14:09:19 +0100

    DML checkpoint

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,5 @@
 --creating tables --
+-- Checkpoint 1 DDL 
 CREATE TABLE Product (
     Product_id VARCHAR2(20) PRIMARY KEY ,
      Product_Name VARCHAR2(20) NOT NULL, 
@@ -18,4 +19,23 @@ CREATE TABLE Orders (
 
 --altering tables --  
 ALTER TABLE Product ADD (Category VARCHAR(20) )
-ALTER TABLE Orders ADD (Order_Date DATE default sysdate)
+ALTER TABLE Orders ADD (Order_Date DATE default sysdate) 
+-- inserting data into tables -- 
+
+-- checkpoint 2 DDM -- 
+
+-- insert in Product --
+INSERT INTO Product values (
+    'P01','Samsung Galaxy S20','Smartphpne',3299 )
+INSERT INTO Product values ( 
+    'P02','ASUS Notebook','PC',4599 ) 
+-- insert in Customer --
+INSERT INTO Customer values (
+    'C01','ALI',71321009)
+INSERT INTO Customer values(
+    'C02','ASMA',77345823)
+-- insert in orders --
+INSERT INTO Orders values (
+    'C01','P02',NULL,2,9198)
+INSERT INTO Orders values(
+    'C02','P01',28/05/2020,1,3299)
